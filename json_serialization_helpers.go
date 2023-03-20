7d7cb105fd3023a1fc48cb27a3263007b55814da
eric-millin <110051399+eric-millin@users.noreply.github.com>
2023-03-20 11:18:14 -0400

    Add Unmarshal and Marshal helpers (#71)
    * Add Unmarshal and Marshal helpers
    
    * Use default factories for Un/Marshal

---
+++ json_serialization_helpers.go
@@ -0,0 +1,50 @@
+package jsonserialization
+
+import (
+	"encoding/json"
+	"reflect"
+
+	absser "github.com/microsoft/kiota-abstractions-go/serialization"
+)
+
+// Unmarshal parses JSON-encoded data using a ParsableFactory and stores it in the value pointed to by model.
+func Unmarshal[T absser.Parsable](data []byte, model *T, parser absser.ParsableFactory) error {
+	jpn, err := absser.DefaultParseNodeFactoryInstance.GetRootParseNode("application/json", data)
+	if err != nil {
+		return err
+	}
+
+	v, err := jpn.GetObjectValue(parser)
+	if err != nil {
+		return err
+	}
+
+	if v != nil {
+		*model = v.(T)
+	} else {
+		// hand off to the std library to set model to its zero value
+		return json.Unmarshal(data, model)
+	}
+
+	return nil
+}
+
+// Marshal JSON-encodes a Parsable value.
+func Marshal(v absser.Parsable) ([]byte, error) {
+	if vRef := reflect.ValueOf(v); !vRef.IsValid() || vRef.IsNil() {
+		return []byte("null"), nil
+	}
+
+	serializer, err := absser.DefaultSerializationWriterFactoryInstance.GetSerializationWriter("application/json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer serializer.Close()
+
+	if err := v.Serialize(serializer); err != nil {
+		return nil, err
+	}
+
+	return serializer.GetSerializedContent()
+}
